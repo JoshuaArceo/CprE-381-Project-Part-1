8420c6ef041572a312a232c0d08f2469e9b20363
jarceo <jarceo@iastate.edu>
2024-10-16 12:20:28 -0500

    Removed clock from barrel shift

--- Barrel_Shifter/shifter.vhd
+++ Barrel_Shifter/shifter.vhd
@@ -10,7 +10,7 @@ entity shifter is
         i_D     : in    std_logic_vector(N-1 downto 0);
         i_AMT     : in    std_logic_vector(4 downto 0);
         i_DIR   : in    std_logic;
-        i_CLK   : in    std_logic;
+        -- i_CLK   : in    std_logic;
         o_Q     : out   std_logic_vector(N-1 downto 0)
     );
 
@@ -21,20 +21,15 @@ architecture dataflow of shifter is
     -- Dir 1 = right
     -- Dir 0 = left
 begin
-process(i_D, i_AMT, i_DIR, i_CLK)
+process(i_D, i_AMT, i_DIR)
 begin
    
-    -- on rising edge
-    if rising_edge(i_CLK) then
         if i_DIR = '1' then
             o_Q <= std_logic_vector(shift_right(unsigned(i_D), to_integer(unsigned(i_AMT))));
         else
             o_Q <= std_logic_vector(shift_left(unsigned(i_D), to_integer(unsigned(i_AMT))));
         end if;
 
-    else o_Q <= o_Q;
-
-    end if;
     
     end process;
 
--- Barrel_Shifter/tb_shifter.vhd
+++ Barrel_Shifter/tb_shifter.vhd
@@ -3,20 +3,20 @@ use IEEE.std_logic_1164.all;
 
 
 entity tb_shifter is
-    generic(gCLK_HPER   : time := 10 ns);
+    -- generic(gCLK_HPER   : time := 10 ns);
 end tb_shifter;
 
 
 
 architecture behavior of tb_shifter is
-    constant cCLK_PER : time := gCLK_HPER *2;
+    -- constant cCLK_PER : time := gCLK_HPER *2;
 
     component shifter is 
     port(
         i_D     : in    std_logic_vector(31 downto 0);
         i_AMT     : in    std_logic_vector(4 downto 0);
         i_DIR   : in    std_logic;
-        i_CLK   : in    std_logic;
+        -- i_CLK   : in    std_logic;
         o_Q     : out   std_logic_vector(31 downto 0)
     );
     end component;
@@ -24,7 +24,7 @@ architecture behavior of tb_shifter is
     signal s_D : std_logic_vector(31 downto 0);
     signal s_Q : std_logic_vector(31 downto 0);
     signal s_AMT : std_logic_vector(4 downto 0);
-    signal s_CLk   : std_logic;
+    -- signal s_CLk   : std_logic;
     signal s_DIR : std_logic;
 
 
@@ -36,39 +36,43 @@ port map(
      i_D => s_D,
      i_AMT => s_AMT,
      i_DIR => s_DIR,
-    i_CLK => s_CLK,
+    -- i_CLK => s_CLK,
      o_Q => s_Q);
 
 
-P_CLK: process
-begin
-s_CLK <= '0';
-wait for gCLK_HPER;
-s_CLK <= '1';
-wait for gCLK_HPER;
-end process;
+-- P_CLK: process
+-- begin
+-- s_CLK <= '0';
+-- wait for gCLK_HPER;
+-- s_CLK <= '1';
+-- wait for gCLK_HPER;
+-- end process;
 
 P_TEST_CASES: process
 begin 
     s_D <= "00000000000000000000000011111111";
     s_AMT <= "00001";
     s_DIR <= '1';
-    wait for cCLK_PER; 
-    
+    -- wait for cCLK_PER; 
+    wait for 10ns;
+
     s_D <= "00000000000000000000000011111111";
     s_AMT <= "00001";
     s_DIR <= '0';
-    wait for cCLK_PER;
+    -- wait for cCLK_PER;
+    wait for 10ns;
 
     s_D <= "10101010101010101010101010101010";
     s_AMT <= "00011";
     s_DIR <= '1';
-    wait for cCLK_PER;
+    -- wait for cCLK_PER;
+    wait for 10ns;
 
     s_D <= "10101010101010101010101010101010";
     s_AMT <= "00011";
     s_DIR <= '0';
-    wait for cCLK_PER;
+    -- wait for cCLK_PER;
+    wait for 10ns;
 
 end process;
 

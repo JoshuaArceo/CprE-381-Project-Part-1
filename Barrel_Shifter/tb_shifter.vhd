c948d975799b0a5daa8d83d7b2be5968d6d8f879
jarceo <jarceo@iastate.edu>
2024-10-21 23:17:10 -0500

    Converted Shifter to structural

--- Barrel_Shifter/tb_shifter.vhd
+++ Barrel_Shifter/tb_shifter.vhd
@@ -16,7 +16,7 @@ architecture behavior of tb_shifter is
         i_D     : in    std_logic_vector(31 downto 0);
         i_AMT     : in    std_logic_vector(4 downto 0);
         i_DIR   : in    std_logic;
-        -- i_CLK   : in    std_logic;
+        i_ARITH : in    std_logic;
         o_Q     : out   std_logic_vector(31 downto 0)
     );
     end component;
@@ -24,7 +24,7 @@ architecture behavior of tb_shifter is
     signal s_D : std_logic_vector(31 downto 0);
     signal s_Q : std_logic_vector(31 downto 0);
     signal s_AMT : std_logic_vector(4 downto 0);
-    -- signal s_CLk   : std_logic;
+    signal s_ARITH: std_logic;
     signal s_DIR : std_logic;
 
 
@@ -36,44 +36,39 @@ port map(
      i_D => s_D,
      i_AMT => s_AMT,
      i_DIR => s_DIR,
-    -- i_CLK => s_CLK,
+     i_ARITH=> s_ARITH,
      o_Q => s_Q);
 
 
--- P_CLK: process
--- begin
--- s_CLK <= '0';
--- wait for gCLK_HPER;
--- s_CLK <= '1';
--- wait for gCLK_HPER;
--- end process;
-
 P_TEST_CASES: process
 begin 
+    s_ARITH <= '0';
+
     s_D <= "00000000000000000000000011111111";
     s_AMT <= "00001";
     s_DIR <= '1';
-    -- wait for cCLK_PER; 
     wait for 10ns;
 
     s_D <= "00000000000000000000000011111111";
     s_AMT <= "00001";
     s_DIR <= '0';
-    -- wait for cCLK_PER;
     wait for 10ns;
 
     s_D <= "10101010101010101010101010101010";
     s_AMT <= "00011";
     s_DIR <= '1';
-    -- wait for cCLK_PER;
     wait for 10ns;
 
     s_D <= "10101010101010101010101010101010";
     s_AMT <= "00011";
     s_DIR <= '0';
-    -- wait for cCLK_PER;
     wait for 10ns;
 
+    s_D <= "10101010101010101010101010101010";
+    s_AMT <= "00011";
+    s_DIR <= '0';
+    s_ARITH <= '1';
+    wait for 10ns;
 end process;
 
 end behavior;
